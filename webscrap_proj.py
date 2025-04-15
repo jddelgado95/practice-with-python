c5b8d0559c6357a30dc1e3b7f2f010d2a8983ac5
jddelgado95 <jddelgado95@gmail.com>
2025-04-15 17:10:02 -0600

    Added sort function to sort by points

--- webscrap_proj.py
+++ webscrap_proj.py
@@ -13,6 +13,9 @@ link = soup.select('.titleline') ##grabs the first element  titleline
 subtext = soup.select('.subtext')
 ##print(votes[0])
 
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key = lambda k:k['votes'], reverse=True)
+
 def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(link):
@@ -23,6 +26,6 @@ def create_custom_hn(links, subtext):
             points = int(vote[0].getText().replace(' points',''))
             if points > 99:
                 hn.append({'title': title, 'link': href,'votes':points})
-    return hn
+    return sort_stories_by_votes(hn)
 
 pprint.pprint(create_custom_hn(link, subtext))
