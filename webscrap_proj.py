0c9e6c714f1fc53280c1a64b0ae70b600c801bc7
jddelgado95 <jddelgado95@gmail.com>
2025-04-15 17:05:15 -0600

    Added pprint format and filtered points in feed news

--- webscrap_proj.py
+++ webscrap_proj.py
@@ -4,21 +4,25 @@
 
 import requests ##Allowd us to donwload the HTML
 from bs4 import BeautifulSoup ##allows to manipulate the HTMl and grab data
+import pprint
+
+
 res = requests.get('https://news.ycombinator.com/news') ##gets the information 
 soup = BeautifulSoup(res.text,'html.parser')
 link = soup.select('.titleline') ##grabs the first element  titleline
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
 ##print(votes[0])
 
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(link):
-
         title = link[idx].getText()
-        href = link[idx].get('href', None)
-        points = int(votes[idx].getText().replace(' points',''))
-        print(points)
-        hn.append({'title': title, 'link': href})
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):            
+            points = int(vote[0].getText().replace(' points',''))
+            if points > 99:
+                hn.append({'title': title, 'link': href,'votes':points})
     return hn
 
-print(create_custom_hn(link, votes))
+pprint.pprint(create_custom_hn(link, subtext))
