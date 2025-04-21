b930e2192a3c4e68a817bf3c2fcb1d4f395859b8
jddelgado95 <jddelgado95@gmail.com>
2025-04-21 17:44:03 -0600

    Added ability to scrap 2 pages

--- webscrap_proj.py
+++ webscrap_proj.py
@@ -7,11 +7,18 @@ from bs4 import BeautifulSoup ##allows to manipulate the HTMl and grab data
 import pprint
 
 
-res = requests.get('https://news.ycombinator.com/news') ##gets the information 
+res = requests.get('https://news.ycombinator.com/news') ##gets the information
+res2 = requests.get('https://news.ycombinator.com/news?p=2') ## here we want to scrap two pages of the webpage
 soup = BeautifulSoup(res.text,'html.parser')
+soup2 = BeautifulSoup(res2.text,'html.parser')
 link = soup.select('.titleline') ##grabs the first element  titleline
 subtext = soup.select('.subtext')
+link2 = soup2.select('.titleline') ##grabs the first element  titleline
+subtext2 = soup2.select('.subtext')
 ##print(votes[0])
+
+mega_links = link + link2
+mega_subtext = subtext + subtext2
 
 def sort_stories_by_votes(hnlist):
     return sorted(hnlist, key = lambda k:k['votes'], reverse=True)
@@ -28,4 +35,4 @@ def create_custom_hn(links, subtext):
                 hn.append({'title': title, 'link': href,'votes':points})
     return sort_stories_by_votes(hn)
 
-pprint.pprint(create_custom_hn(link, subtext))
+pprint.pprint(create_custom_hn(mega_links, mega_subtext))
