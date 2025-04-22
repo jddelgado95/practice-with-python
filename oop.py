d980826483ea5c9501f2d4ebfeba995fcc047019
jddelgado95 <jddelgado95@gmail.com>
2025-04-21 18:02:13 -0600

    Create object, only blueprint

---
+++ oop.py
@@ -0,0 +1,17 @@
+## Python advanced OOP 
+## Everything in Python is an object
+## An object has attributes and methods
+## We can create our own data types using classes and objects, with their own attributes and methods
+## is a paradigm, it enforces us to think about our code, its purpose, and makes us structure it. 
+
+# convention: camelcase for classes
+class BigObject: ## this class is only a blueprint
+    # code
+    pass
+
+obj1 = BigObject() #this is the actual object, which is an instance of the class. 
+print(type(obj1))
+
+
+
+
