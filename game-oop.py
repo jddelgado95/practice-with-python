e2f2bad4050da67124733ed65d689ba4204a84ea
jddelgado95 <jddelgado95@gmail.com>
2025-04-25 08:51:55 -0600

    Add couple of lines for object introspection

--- game-oop.py
+++ game-oop.py
@@ -3,13 +3,17 @@
 ## Polymorphism is another pillar of OOP. Classes and objects can share methods, but they not necessarily works the same way from one class to another. 
 
 class User(object): 
+    def __init__(self, email):
+        self.email = email
     def sign_in(self):
         print('logged in')
     def attack(self):
         print('Do nothing') ## this method will be overwrite by the other attack methods from child classes. That's polymorphism. 
 
 class Wizard(User): ## pass the parent class
-    def __init__(self, name, power):
+    def __init__(self, name, power, email):
+        #User.__init__(self, email) ## we can do this, but there's a better method. User super
+        #super().__init__(self, email) ## referes to the super class, the class above Wizard, which is User. The super() function is used to give access to methods and properties of a parent or sibling class. The super() function returns an object that represents the parent class
         self.name = name 
         self.power = power
     
@@ -26,7 +30,7 @@ class Archer(User):
 
 ## built-in Python function that checks if an object is an instance of a specified class or a tuple of classes. It returns True if the object matches the type, and False otherwise. It is commonly used for type checking, offering flexibility by supporting inheritance and multiple types
 
-wizard1 = Wizard('Merlin',50)
+wizard1 = Wizard('Merlin',50, 'merlin@gmail.com')
 archer1 = Archer('Robin',100)
 ##print(isinstance(wizard1, Wizard)) ## should print TRUE
 ##print(isinstance(wizard1, User)) ## should print TRUE
@@ -35,8 +39,14 @@ archer1 = Archer('Robin',100)
 ##print(isinstance(wizard1, object)) ##should print TRUE
 #wizard1.attack()
 #archer1.attack()
-def player_attack(char):
-    char.attack()
-player_attack(wizard1) ## both functions works the same but results are not the same. 
-player_attack(archer1)
+#def player_attack(char):
+    #char.attack()
+#player_attack(wizard1) ## both functions works the same but results are not the same. 
+#player_attack(archer1)
+#print(wizard1.email) ## prints an error because even if we might inherit, we can not inherit a constructor this easy. We can add an email attribute to the init in the wizard class, but that's not efficient. 
+## we =want to call the init function from user within the wizard class
+
+
+#instrospection: is the ability to know the type of an object at runtime. 
+#print(dir(wizard1)) ## gives all the methods and attributes of Wizard() class.
 
